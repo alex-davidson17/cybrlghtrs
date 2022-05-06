60e46fdf7df7a967779d22ceaa42562e6d3670ab
Alyssa Davidson <alyssa.davidson17@gmail.com>
2022-05-05 22:01:07 -0400

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // add extra fonts here
+      },
+      colors: {
+        "light-green": "#d0f4de",
+        "light-blue": "#a9def9",
+        "light-purple": "#e4c1f9",
+        "light-yellow": "#fcf6bd",
+        pink: "#ff99c8",
+      },
+    },
   },
   plugins: [],
 };
