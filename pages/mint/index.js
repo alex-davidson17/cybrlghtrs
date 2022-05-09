5be096d118b28a9f7a66c9c6794be7017cfbe17d
Alyssa Davidson <alyssa.davidson17@gmail.com>
2022-05-08 21:34:25 -0400

    added main image and counter skeleton

--- pages/mint/index.js
+++ pages/mint/index.js
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import Head from "next/head";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWindowMinimize,
   faWindowMaximize,
   faX,
+  faMinus,
+  faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Mint() {
@@ -33,9 +38,35 @@ export default function Mint() {
               <li>Options</li>
             </ul>
           </div>
-          <div className="h-60 w-auto p-4 text-center">
-            <div className="h-5/6 w-auto bg-white">
+          <div className="h-full w-auto p-4 text-center">
+            <div className="h-5/6 w-auto bg-white overflow-hidden">
               <h2>Presale</h2>
+              <div className="flex flex-col md:flex-row md:space-x-14 w-full mt-10 md:mt-14">
+                <div className="relative">
+                  <div className="absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-brand-purple rounded-md flex items-center justify-center text-white font-semibold">
+                    <p>
+                      <span className="text-pink">0</span> / 3000
+                    </p>
+                  </div>
+
+                  <img
+                    src="/images/main-lighter.png"
+                    className="object-cover w-full sm:h-[280px] md:w-[250px] rounded-md"
+                  />
+                </div>
+                <div className="flex flex-row items-center justify-center">
+                  <button>
+                    <FontAwesomeIcon
+                      icon={faMinus}
+                      size="2xl"
+                    ></FontAwesomeIcon>
+                  </button>
+                  <p className="text-5xl">1</p>
+                  <button>
+                    <FontAwesomeIcon icon={faPlus} size="2xl"></FontAwesomeIcon>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
